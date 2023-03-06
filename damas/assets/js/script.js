e2065c66a187f030c53979a6186c4255afa9d8e0
sandersonrafael <sandersonrafael-35@hotmail.com>
2023-03-05 22:10:15 -0300

    Att

--- damas/assets/js/script.js
+++ damas/assets/js/script.js
@@ -1,10 +1,26 @@
+document.querySelectorAll('.inicio1').forEach((inicio1) => inicio1.innerHTML = '⨷')
+document.querySelectorAll('.inicio2').forEach((inicio2) => inicio2.innerHTML = '⊛')
+
 let elementos = document.querySelectorAll('.branco')
-console.log(elementos)
-document.querySelectorAll('.inicio1').forEach((inicio1) => inicio1.innerHTML = '⊜')
-document.querySelectorAll('.inicio2').forEach((inicio2) => inicio2.innerHTML = '⊗')
+let peca = ''
 
 elementos.forEach((espaco) => {
+    espaco.setAttribute('style', 'background-color: white')
     espaco.addEventListener('click', function() {
-        // this.innerHTML === '◎' ? this.innerHTML = '' : this.innerHTML = '◯'
+        this.style.backgroundColor = 'rgba(255, 0, 0, 0.151)'
+        
+        if (this.innerHTML !== '') {
+            if (peca === '') peca = this.innerHTML
+        }
+        if (this.innerHTML === '') {
+            if (peca !== '') this.innerHTML = peca
+        }
+
+        peca = this.innerHTML
+        // if (this.innerHTML === peca) this.innerHTML = ''
+        setTimeout(() => {
+            this.style.backgroundColor = 'white'
+            peca = ''
+        }, 800)
     })
 })
